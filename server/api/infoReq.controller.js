1e07c5b3cc59318208d2ea9183eab0a6b3258b43
Léa Baty <leabaty.dev@gmail.com>
2022-04-07 12:14:01 +0200

    Add: Email templates on server side

--- server/api/infoReq.controller.js
+++ server/api/infoReq.controller.js
@@ -77,7 +77,7 @@ export default class InfoReqController {
       let mailOptions = {
         from: process.env.MAIL_FROM,
         to: formData.email,
-        subject: "🧘 Votre demande d'information à Laure Pépin | Apsara Yoga",
+        subject: "👩‍💻 Thank you for your message ! | Léa Baty's Portfolio",
         template: "infoRecap",
         context: {
           firstname: formData.firstname,
