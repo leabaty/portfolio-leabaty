3d9394c7406313d06dc3eb0c5469d82c609e35b5
Léa Baty <leabaty.dev@gmail.com>
2022-11-27 20:31:49 +0100

    Update API URL

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-import app from "./app.js";
-import dotenv from "dotenv";
+import app from './app.js';
+import dotenv from 'dotenv';
 
 //CONFIG
 dotenv.config();
 
 // DEFINING THE PORT
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || '8080';
 
 app.listen(port, () => console.log(`Server is istening on port ${port}`));
